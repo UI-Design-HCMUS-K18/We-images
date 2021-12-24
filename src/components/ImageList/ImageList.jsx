b18e72ed7a52e104d985e34e93804521371e11dc
Le Quoc Dat <18120305@student.hcmus.edu.vn>
2021-12-25 00:09:57 +0700

    image hover

--- src/components/ImageList/ImageList.jsx
+++ src/components/ImageList/ImageList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import unsplash from '../../api/unsplash';
 import ImageCard from '../ImageCard/ImageCard';
+import CircularProgress from '@mui/material/CircularProgress';
 import { useLocation } from 'react-router-dom';
 import './imagelist.css';
 
@@ -8,16 +9,23 @@ function ImageList(props) {
   const search = useLocation().search;
   const query = new URLSearchParams(search).get('query');
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true);
     (async () => {
       const response = await unsplash.get('/search/photos', {
         params: { query: query, page: 1, per_page: 30 },
       });
       setImages(response.data.results);
+      setLoading(false);
     })();
   }, [query]);
-  if (images.length === 0) {
-    return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <CircularProgress />
+      </div>
+    );
   }
   return (
     <div className="imageList">
